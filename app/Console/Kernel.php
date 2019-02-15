36ddfd9d7c9e7c4318fa9c3c1de931430c5721c5
Herbert Balagtas <hbalagta@uwaterloo.ca>
2019-02-14 22:44:00 -0500

    Add scheduled commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('jijiki:processfeeds')->everyFiveMinutes();
+        $schedule->command('jijiki:processads')->everyFifteenMinutes();
     }
 
     /**
