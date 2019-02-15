1eb74b15989b4874d487d0d9d1b7b25d831731d1
Herbert Balagtas <hbalagtas@gmail.com>
2019-02-15 12:49:14 -0500

    Remove default laravel homepage

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
-	Route::get('/home', 'HomeController@index')->name('home');
+	Route::get('/', 'HomeController@index')->name('home');
 
 	Route::resource('feed', 'FeedController');
 });
