c0ae296e513acd7187899b74d328c6c22a099bce
Herbert Balagtas <hbalagtas@gmail.com>
2019-02-18 17:23:02 -0500

    Add exception emailer

---
+++ app/Mail/ExceptionOccured.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jijiki\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ExceptionOccured extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+       return $this->view('emails.exception')
+                    ->from('hbalagtas@live.com')
+                    ->with('content', $this->content);
+    }
+}
